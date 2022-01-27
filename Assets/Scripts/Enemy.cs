bf21d36377e9d70584b58339b817ae08bdb701f7
Jinle Chen <cjinle@gmail.com>
2022-01-27 16:13:06 +0800

    enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,25 +8,35 @@ public class Enemy : MonoBehaviour
     [SerializeField] Sprite[] sprites;
     [SerializeField] SpriteRenderer curSprite;
 
+    [SerializeField] float speed;
+
     void Start()
     {
         
     }
     void Update()
     {
-        
+        var step = speed * Time.deltaTime;
+        transform.Translate(-step, 0, 0);
+
+        var sp = Camera.main.WorldToScreenPoint(transform.position);
+        if (sp.x < -Screen.width/2)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void SetType(int idx)
     {
-        if (idx > sprites.Length)
+        if (idx >= sprites.Length)
         {
             idx %= sprites.Length;
         }
         curSprite.sprite = sprites[idx];
-        float maxWidth = Screen.width / 2;
-        float maxHeight = Screen.height / 2;
-        var pos = new Vector3(Random.Range(-maxWidth, maxWidth), Random.Range(-maxHeight, maxHeight), 12);
-        transform.position = Camera.main.ScreenToWorldPoint(pos+Vector3.right);
+        transform.position = new Vector3(8f, Random.Range(-5f, 5f), 12);
+        //float maxWidth = Screen.width / 2;
+        //float maxHeight = Screen.height / 2;
+        //var pos = new Vector3(Random.Range(-maxWidth, maxWidth), Random.Range(-maxHeight, maxHeight), 12);
+        //transform.position = Camera.main.ScreenToWorldPoint(pos+Vector3.right);
     }
 }
